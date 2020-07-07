20f526e450e13c844f4b58e5c088512a7a533634
sugiarto <sugiarto@gie-art.com>
2020-07-08 05:59:55 +0700

    added: create, read, update, delete product

---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/shopspring/decimal"
+)
+
+var db *gorm.DB
+var err error
+
+// Product is a representation of a product
+type Product struct {
+	ID    int             `form:"id" json:"id"`
+	Code  string          `form:"code" json:"code"`
+	Name  string          `form:"name" json:"name"`
+	Price decimal.Decimal `form:"price" json:"price" sql:"type:decimal(16,2);"`
+}
+
+// Result is an array of product
+type Result struct {
+	Code    int         `json:"code"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+// Main
+func main() {
+	db, err = gorm.Open("mysql", "root:@/go_api_crud?charset=utf8&parseTime=True")
+
+	if err != nil {
+		log.Println("Connection failed", err)
+	} else {
+		log.Println("Connection established")
+	}
+
+	db.AutoMigrate(&Product{})
+	handleRequests()
+}
+
+func handleRequests() {
+	log.Println("Start the development server at http://127.0.0.1:9999")
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/api/products", createProduct).Methods("POST")
+	myRouter.HandleFunc("/api/products", getProducts).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+	myRouter.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
+	myRouter.HandleFunc("/api/products/{id}", deleteProduct).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":9999", myRouter))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome!")
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var product Product
+	json.Unmarshal(payloads, &product)
+	db.Create(&product)
+
+	// price, _ := decimal.NewFromString("5000000.00")
+	// db.Create(&Product{Code: "AB234234", Name: "Sepeda polygon", Price: price})
+
+	fmt.Println(product)
+	fmt.Println("Endpoint hit: create new product")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit: get products")
+
+	products := []Product{}
+	db.Find(&products)
+
+	res := Result{Code: 200, Data: products, Message: "Success get products"}
+	results, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(results)
+
+	// json.NewEncoder(w).Encode(products)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	var product Product
+
+	db.First(&product, productID)
+
+	res := Result{Code: 200, Data: product, Message: "Success get product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	payloads, _ := ioutil.ReadAll(r.Body)
+
+	var productUpdates Product
+	json.Unmarshal(payloads, &productUpdates)
+
+	var product Product
+	db.First(&product, productID)
+	db.Model(&product).Updates(productUpdates)
+
+	res := Result{Code: 200, Data: product, Message: "Success update product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	var product Product
+
+	db.First(&product, productID)
+	db.Delete(&product)
+
+	res := Result{Code: 200, Message: "Success delete product"}
+	result, err := json.Marshal(res)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+}
